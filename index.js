8d39fc9c24eb2c4f46728e416226547b0a53965f
Jayesh Choudhary <jayeshchoudhary@Jayeshs-MacBook-Pro.local>
2022-06-11 22:36:41 +0530

    fix bugs

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 const fs = require("fs");
-const rl = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const readLine = require("readline");
 
-function printAllTodos() {
-  console.log("-> All the todos\n");
+function readInput(question) {
+  return new Promise(function (resolve) {
+    let userInput = null;
+    const rl = readLine.createInterface(process.stdin, process.stdout);
+    rl.setPrompt(question + " : ");
+    rl.prompt();
+    rl.on("line", function (input) {
+      userInput = input;
+      rl.close();
+    }).on("close", function () {
+      resolve(userInput); // this is the final result of the function
+    });
+  });
+}
 
+function getTodosList() {
   try {
     const data = fs.readFileSync("todo.json", "utf8");
     const todoArray = JSON.parse(data);
-    todoArray.forEach((todo) => {
-      console.log(
-        `${todo.id}. ${todo.title} ${todo.isCompleted ? "(Done)" : ""}`
-      );
-    });
+    return todoArray;
   } catch (err) {
     console.error(err);
   }
+}
+
+function printAllTodos() {
+  console.log("-> All the todos\n");
+  const todoArray = getTodosList();
+
+  if (!todoArray.length) {
+    console.log("No todos here\n");
+    return;
+  }
+
+  todoArray.forEach((todo) => {
+    console.log(
+      `${todo.id}. ${todo.title} ${todo.isCompleted ? "(Done)" : ""}`
+    );
+  });
+
   console.log();
 }
 
@@ -29,23 +52,78 @@ function printMenu() {
 }
 
 async function acceptOperation() {
-  await new Promise((resolve) => {
-    rl.question("What is Op number? ", (opn) => {
-      if (["1", "2", "3"].includes(opn)) {
-        return opn;
-      } else {
-        acceptOperation();
-      }
-    });
-  });
+  const selectedOption = await readInput("Enter option number");
+  if (["1", "2", "3"].includes(selectedOption)) {
+    return selectedOption;
+  }
+  console.log("Wrong option selected, retry again\n");
+  return acceptOperation();
 }
 
-function app() {
-  console.log("********* Welcome to Todo App **********\n");
+async function addTodo() {
+  const title = await readInput("Enter todo title");
+
+  const todoList = getTodosList();
+
+  todoList.push({ id: todoList.length + 1, title, isCompleted: false });
+
+  writeFileContents(JSON.stringify(todoList));
+}
+
+function writeFileContents(contents) {
+  try {
+    fs.writeFileSync("todo.json", contents);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function markTodoDone() {
+  const todoId = await readInput(
+    "Enter todo id which you want to mark complete"
+  );
+
+  if (isNaN(todoId)) {
+    // todoid is not a number
+    console.log("Todo id should be number, retry again\n");
+    return markTodoDone();
+  }
+
+  const todoList = getTodosList();
+
+  if (+todoId < 0 || +todoId > todoList.length) {
+    // todoid does not exist
+    console.log("Todo id does not exist, retry again\n");
+    return markTodoDone();
+  }
+
+  todoList[+todoId - 1].isCompleted = true;
+
+  writeFileContents(JSON.stringify(todoList));
+}
+
+async function performOperation(choice) {
+  switch (choice) {
+    case 1:
+      await addTodo();
+      return;
+    case 2:
+      await markTodoDone();
+      return;
+    case 3:
+      console.log("\nBye!, see you soon.");
+      process.exit(1);
+  }
+}
+
+async function app() {
+  console.log();
   printAllTodos();
   printMenu();
-  const selectedOperation = acceptOperation();
-  rl.close();
+  const selectedOperation = await acceptOperation();
+  await performOperation(+selectedOperation);
+  app();
 }
 
+console.log("********* Welcome to Todo App **********");
 app();
